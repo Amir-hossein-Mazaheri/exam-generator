c2fc204bbd84b0d4a1a088c395629d44ed0147de
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-10 17:35:59 +0330

    make 404 page prettier

--- src/Pages/Error/404.js
+++ src/Pages/Error/404.js
@@ -1,8 +1,20 @@
+import { Result } from "antd";
+import { Link } from "react-router-dom";
+import Button from "../../Common/Button";
+
 function NotFoundPage() {
   return (
     <div>
-      404 ERROR {":))"}
-      Fuck off
+      <Result
+        status="404"
+        title="403"
+        subTitle="Sorry, you are not authorized to access this page."
+        extra={
+          <Button className="bg-sky-500 text-white">
+            <Link to="/">خانه</Link>
+          </Button>
+        }
+      />
     </div>
   );
 }
