c976ccf8feaa74fa93fdc706a58339be8b1f9fda
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-11 17:48:19 +0330

    added to utc time helper function

--- src/Helpers/convertToJalali.js
+++ src/Helpers/convertToJalali.js
@@ -1,9 +1,15 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import jalali from "jalaliday";
 import "dayjs/locale/fa";
 
 dayjs.extend(jalali);
+dayjs.extend(utc);
 
 export function showJalaliTime(time, format = "YYYY/MM/DD HH:mm") {
   return dayjs(time).calendar("jalali").locale("fa").format(format);
+}
+
+export function convertToUTC(time) {
+  return dayjs.utc(time).toISOString();
 }
